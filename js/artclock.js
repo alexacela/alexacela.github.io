2c0d8c82b7d0798b9f0a2d9ce133aac388c15b80
Alex Sugurel <alexacela@gmail.com>
2018-03-23 01:57:28 -0400

    added part 3 Art Clock. fixed bugs for page titles in html files

--- js/artclock.js
+++ js/artclock.js
@@ -1,90 +1,76 @@
 //define variables
 let canvasWidth = 1024;
 let canvasHeight = 768;
-//tracking mouse click
-let countMouse = 0;
-// space between the tally marks
-let tallySpace = 20;
-// space between the tally groups
-let groupSpace = 150;
-// tally groups per row
-let rowLenght = 6;
-// tallys per group
-let groupLength = 5;
-// position to start horizontally
-let originX = 100;
-// position to start vertically
-let originY = 130;
-// how long each Tally is
-let tallyLength = 50;
-// height of the rows
-let rowHeight = 120;
+
 
 //create the canvas 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
 }
 
-//function for mouse event
-function mousePressed(){
-	countMouse++;
-}
-
-function keyPressed() {
-	countMouse=0;
-}
-
-
-//define drawing function
+//drawing function
 function draw() {
-	clear();
-	background(0);
-	//set the color the same as the site's background
-	stroke(60,58,112);
-	strokeWeight(1);
-	textSize(50);
-	fill(60,58,112);
-	// display the number of clicks
-	text(countMouse, canvasWidth-100, canvasHeight-30);
+  clear();
+  background(0);
+  stroke(190);
+  strokeWeight(2);
+  fill(60,58,112);
 
-	//compute how many groups we have
-	let groups = int(countMouse / groupLength);
-	let remainder = countMouse % groupLength;
-	if (remainder > 0){
-		groups = groups + 1;
-	}
-	//compute how many rows
-	let rows = int(groups / rowLenght);
-	remainder = groups % rowLenght;
-	if (remainder > 0) {
-		rows = rows + 1;
-	}
-	//draw the Tally lines using nested for loops
-	strokeWeight(5);
-	for (let r = 0; r < rows; r++) {
-		let rowLimit = rowLenght;
-		if (groups - r * rowLenght < rowLenght) {
-			rowLimit = groups - r * rowLenght;
-		}
-		for (let g = 0; g < rowLimit; g++) {
-			let tallyLimit = groupLength; 
-			if (countMouse - r * rowLenght * groupLength - g * groupLength < groupLength){
-				tallyLimit = countMouse - r * rowLenght * groupLength - g * groupLength;
-			}
-			for (let t = 0; t < tallyLimit; t++) {
-				let x1 = originX + g * groupSpace + t * tallySpace;
-				let y1 = originY + r * rowHeight;
-				let x2 = x1;
-				let y2 = y1 - tallyLength;
-				// select the 5th tally and make it diagonal
-				if (t == groupLength - 1) {
-					x1 = x1 - groupLength * tallySpace;
+  let h = hour();
+  let m = minute();
+  let s = second();
 
-				}
-				line(x1, y1,
-					 x2, y2);
-			}
-		}
-	}
+  let hBin = h.toString(2);
+  let mBin = m.toString(2);
+  let sBin = s.toString(2);
+  let size = 100;
+  let on = color(255, 255, 255);
+  let off = color(100, 100, 100);
+
+  push();
+  translate (250, 250);
+
+  //hour 
+  for (let i in hBin) {
+    push();
+    translate(i*size, 0);
+    if(hBin[i]=='1'){
+      fill(on);
+    } else {
+      fill(off);
+    }
+    rect(0, 0, size, size);
+    pop();
+  }
+
+  //minutes
+  for (let i in mBin) {
+    push();
+    translate(i*size, 1.5*size);
+    if(mBin[i]=='1'){
+      fill(on);
+    } else {
+      fill(off);
+    }
+    rect(0, 0, size, size);
+    pop();
+  }
+
+    //seconds
+  for (let i in sBin) {
+    push();
+    translate(i*size, 3*size);
+    if(sBin[i]=='1'){
+      fill(on);
+    } else {
+      fill(off);
+    }
+    rect(0, 0, size, size);
+    pop();
+  }
+
+    pop();
+
+
+
 }
-
